ed045c55ed0f5dffb37a3b3bc7882149b3b179e4
Ronan Lina <ronanlina@gmail.com>
2018-07-02 22:21:19 +0800

    Improved documentation

--- SandboxPractice/practice.py
+++ SandboxPractice/practice.py
@@ -5,16 +5,18 @@ import sys
 
 class Practice():
 
-
+    #A cliche
     def Greet():
         print('HELLO WORLD! \n')
     
+    #Determines whether the number is odd or even.
     def OddEven(x):
         if int(x)%2==0:
             return True
         else:
             return False
     
+    #Swaps variable values
     def Swapper(x,y):
         temp = x
         x = y 
@@ -22,6 +24,7 @@ class Practice():
 
         return x   
 
+    #Computes for the sum of each individual digits of a number
     def SumOfDigits(x):
         num = 0
         for i in range(len(x)):
@@ -29,6 +32,7 @@ class Practice():
         
         return num
     
+    #Reverses the numerical input with no use of any string functions
     def Reverser(x):
         
         reversed = 0
