cda16251e0d269d2a6093aacd33a16d7607706f7
Ronan Lina <ronanlina@gmail.com>
2018-07-03 11:27:15 +0800

    polished more. added menus and methods

--- SandboxPractice/main.py
+++ SandboxPractice/main.py
@@ -2,34 +2,71 @@
 
 import os
 import sys
+import time
 from practice import Practice
 
-Practice.Greet()
-
 #Odd or Even Guesser
-x = input('Enter a number : ')
+def OddEven():
+    x = input('Enter a number : ')
 
-if Practice.OddEven(x) == True:
-    print('even')
-else:
-    print('odd')
+    if Practice.OddEven(x) == True:
+        print('even')
+    else:
+        print('odd')
 
 #Swapper
+def Swapper():
+    x = input('enter number x : ')
+    y = input('enter number y : ')
 
-x = input('enter number x : ')
-y = input('enter number y : ')
+    print('Pre-Swap : {} & {}'.format(x,y))
+    print('Post-Swap : {} & {}'.format(Practice.Swapper(x,y),Practice.Swapper(y,x)))
 
-print('Pre-Swap : {} & {}'.format(x,y))
-print('Post-Swap : {} & {}'.format(Practice.Swapper(x,y),Practice.Swapper(y,x)))
 
 #Sum of Digits
+def SumOfDigits():
 
-x = input("enter a number : ")
+    x = input("enter a number : ")
 
-print('sum : {}'.format(Practice.SumOfDigits(x)))
+    print('sum : {}'.format(Practice.SumOfDigits(x)))
+
 
 #Reverser
+def NumberReverse():
 
-x = input("enter a number : ")
+    x = input("enter a number : ")
 
-print('Reversed number : {}'.format(Practice.Reverser(x)))
+    print('Reversed number : {}'.format(Practice.Reverser(x)))
+
+#Determines wich method to run
+def Selector(Activity):
+    if Activity == 1:
+        OddEven()
+    elif Activity == 2:
+        Swapper()
+    elif Activity == 3:
+        SumOfDigits()
+    elif Activity == 4:
+        NumberReverse()
+    else:
+        os.system('cls')
+        quit()
+    
+
+def PrintMenu():
+    print('[1]. Odd or Even')
+    print('[2]. Swapper')
+    print('[3]. Sum of Digits')
+    print('[4]. Number Reverse')
+    print('[5]. Exit\n')
+
+def main():
+    os.system('cls')
+    Practice.Greet()
+    PrintMenu()
+    Activity = int(input('Select Activity : '))
+    Selector(Activity)
+    time.sleep(2)
+    main()
+
+main()
